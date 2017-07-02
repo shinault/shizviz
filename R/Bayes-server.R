02731058427f4b2780592e798533d5360af64295
GMS <gshinault@gmail.com>
2017-07-02 12:41:37 -0500

    Fix bug in bayes_server

--- R/Bayes-server.R
+++ R/Bayes-server.R
@@ -23,18 +23,18 @@ bayes_sim <- function(prior, tpr, tnr) {
 }
 
 bayes_plots <- function(sim) {
-  ggplot2::ggplot(sim, aes(x=X,y=Y)) +
-    ggplot2::geom_tile(aes(fill=Wellness),colour="white") +
-    ggplot2::theme(panel.grid.major = element_blank(),
-                   panel.grid.minor = element_blank(),
-                   panel.background = element_blank(),
+  ggplot2::ggplot(sim, ggplot2::aes(x=X,y=Y)) +
+    ggplot2::geom_tile(ggplot2::aes(fill=Wellness),colour="white") +
+    ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_blank(),
                    axis.title.x = ggplot2::element_blank(),
                    axis.text.x  = ggplot2::element_blank(),
                    axis.ticks.x = ggplot2::element_blank(),
                    axis.title.y = ggplot2::element_blank(),
                    axis.text.y  = ggplot2::element_blank(),
                    axis.ticks.y = ggplot2::element_blank()) +
-    ggplot2::geom_point(aes(shape=`Test Result`)) +
+    ggplot2::geom_point(ggplot2::aes(shape=`Test Result`)) +
     scale_shape_manual(values=c(4, 32))
 }
 
